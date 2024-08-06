659f336d38840e2a0bbd2b17a5865aa311409965
lunaWewe <Ls22012900@gmail.com>
2024-08-06 15:32:47 +0800

    123

---
+++ LunaJava/src/tw/luna/pretty/Luna33.java
@@ -0,0 +1,13 @@
+package tw.luna.pretty;
+
+import java.io.File;
+
+public class Luna33 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(File.pathSeparator);
+		System.out.println(File.separator);
+	}
+
+}
